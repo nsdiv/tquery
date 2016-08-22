eb55400dfe074b53b5ba503c6ead0b2b6cb760be
Ninad Divadkar <nsdiv@hotmail.com>
2016-08-21 23:23:32 -0700

    roadground require

---
+++ plugins/requirejs/confrequire/roadground.initrequire.js
@@ -0,0 +1,4 @@
+define(['module', 'plugins/roadground/tquery.roadground'], function(module){
+	// set baseUrl for this plugin
+	tQuery.createRoadGround.baseUrl	= module.uri+'/../../../roadground/';
+});
